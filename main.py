352e7e1fcc56cac6d1a56d13861085e20b956916
Arshad Hannan <61543546+ArshadHannan@users.noreply.github.com>
2020-12-08 15:48:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ import time
 import csv
 from selenium.webdriver.chrome.options import Options
 
+
+list_of_titles = []
+
 #disabling notifications
 options = Options()
 options.add_argument("--disable-notifications")
 
-#setting up the chrome driver 
+#setting up the chrome driver
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH, options=options)
 driver.get("https://www.freepatentsonline.com/search.html")
@@ -23,26 +26,42 @@ search.send_keys('PEX/"ROTARU, OCTAVIAN"')
 submit_btn = driver.find_element_by_name("search")
 submit_btn.click()
 
-# open csv
-with open('patent-results.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["patent-title", "patent-number", "patent-issue-date"])
+
+
+
+#open csv for all titles
+with open('patent-titles.csv', 'w', newline='') as file:
+    writer1 = csv.writer(file)
+    writer1.writerow(["patent-titles"])
 
 # for i in range(2, 6):
 
-
-
     # get values
-    try:
 
-            element = WebDriverWait(driver, 10).until(
-                # EC.presence_of_element_located((By.XPATH, '//*[@id="results"]/div[2]/div/div/table/tbody/tr['+str(i)+']/td[3]/a')),
-                EC.presence_of_element_located((By.XPATH, '//*[@id="results"]/div[2]/div/div/table/tbody/tr[3]/td[3]/a'))
+    try:
+            #getting all titles
+            titles = WebDriverWait(driver, 10).until(
+                EC.presence_of_all_elements_located(
+                    (By.XPATH, '//*[@id="results"]/div[2]/div/div/table/tbody/tr/td[3]/a'))
             )
-            element.click()
+            for value in titles:
+                print(value.text)
+                list_of_titles.append(value.text+"\n")
+            print(list_of_titles)
+            writer1.writerow(list_of_titles)
+
+
+            link_submit = driver.find_elements_by_xpath('//*[@id="results"]/div[2]/div/div/table/tbody/tr[3]/td[3]/a')[0]
+            link_submit.click()
+
+                # EC.presence_of_element_located((By.XPATH, '//*[@id="results"]/div[2]/div/div/table/tbody/tr['+str(i)+']/td[3]/a')),
+                # EC.presence_of_element_located((By.XPATH, '//*[@id="results"]/div[2]/div/div/table/tbody/tr[3]/td[3]/a'))
+                #
+                # element.click()
+
 
             # accessing the details
-            
+
             patent_title = WebDriverWait(driver, 10).until(
                 EC.presence_of_element_located((By.CLASS_NAME, "disp_elm_text"))
             )
@@ -62,7 +81,10 @@ with open('patent-results.csv', 'w', newline='') as file:
             print(patent_result)
 
             # write it to the csv
-            writer.writerow(patent_result)
+            with open('patent-results.csv', 'w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(["patent-title", "patent-number", "patent-issue-date"])
+                writer.writerow(patent_result)
 
 
 
