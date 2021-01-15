ab25174c990478a3c3f27e162261f93446b7acef
ashenicas <ashenicas@gmail.com>
2021-01-16 01:25:03 +0530

    email multiple receivers

--- back_end/helpers/send_mail.py
+++ back_end/helpers/send_mail.py
@@ -7,20 +7,24 @@ class SendMail:
     def __init__(self):
         self.sender_email = "jerrypatents@gmail.com"
         self.sender_email_password = "jerrypatents123"
-        self.receiver_email = "ashenicas@gmail.com"
+        self.receiver_emails = ["madhawadias2@gmail.com", "ashenicas@gmail.com"]
 
     def runner(self, file_url, file_name):
         text = "Please refer following link to download the search result of {}  : {}".format(file_name, file_url)
+        receive = ",".join(self.receiver_emails)
 
         msg = MIMEText(text)
         msg['Subject'] = 'Patent Extraction result of {}'.format(file_name)
         msg['From'] = self.sender_email
-        msg['To'] = self.receiver_email
+        msg['To'] = receive
         print("Email Body", msg)
         s = smtplib.SMTP('smtp.gmail.com', 587)
         s.starttls()
         s.ehlo()
         s.login(self.sender_email, self.sender_email_password)
-        s.sendmail(self.sender_email, self.receiver_email, msg.as_string())
+        s.sendmail(self.sender_email, self.receiver_emails, msg.as_string())
         print("Email Sent")
         s.quit()
+
+
+SendMail().runner(file_name="rotaru", file_url="test url")
