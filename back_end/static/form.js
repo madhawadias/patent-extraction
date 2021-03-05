3c90d8f979a679cb28079478e3973c963ecdcf76
ashenicas <ashenicas@gmail.com>
2021-03-05 22:55:06 +0530

    clickable link

--- back_end/static/form.js
+++ back_end/static/form.js
@@ -53,8 +53,8 @@ $(document).ready(function() {
 				if(data.name.link) {
 					$('#successAlert').hide();
 				    $('#successAlertWithLink').text(data.name.text).show();
-                    $("#download_link").attr("href", data.name.link);
-                    $("#download_link").text(data.name.link);
+                    $("a#download_link").attr("href", data.name.link);
+                    $("a#download_link").text(data.name.link);
 				}
 				else{
 				    $('#successAlert').text(data.name.text).show();
