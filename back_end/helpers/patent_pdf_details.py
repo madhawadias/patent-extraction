a9e5d952f99488ba04dbd26261b73e5b2f96f181
Arshad Hannan <arshad.2018197@iit.ac.lk>
2021-06-20 10:47:19 +0530

    increase sleep time on image file wrapper

--- back_end/helpers/patent_pdf_details.py
+++ back_end/helpers/patent_pdf_details.py
@@ -103,7 +103,7 @@ class PatentDownload:
                     image_file_wrapper = WebDriverWait(driver, 40).until(
                         EC.presence_of_element_located((By.ID, "imageFileWrapperId"))
                     )
-                    time.sleep(3)
+                    time.sleep(10)
                     image_file_wrapper.click()
 
                 except Exception as e:
